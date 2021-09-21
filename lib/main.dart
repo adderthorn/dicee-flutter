ae2c24f003a2b125a5eeb2c8e18d3648b28676a0
Noah Wood <noah@noahw.org>
2021-09-21 14:40:08 -0400

    Update AppFrameworkInfo.plist, flutter_export_environment.sh, and 4 more files...

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+const String _title = 'Dicee';
+
 void main() {
   return runApp(
     MaterialApp(
+      title: _title,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text(_title),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +21,24 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: (){
+                print('test');
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
